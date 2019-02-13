8a4e0cbf67ea2f6d99cf696da272acf588a4fb8c
Timothy <tsnow20@me.com>
2019-02-12 17:08:17 -0700

    Replace getopt with argparse Removed standard usage() method, will utilize built in help generated by argparse.

--- nagpymail.py
+++ nagpymail.py
@@ -1,73 +1,53 @@
 #!/usr/bin/python
-import getopt
+import argparse
 import sys
 import smtplib
 from email.utils import formatdate
 from email.mime.text import MIMEText
 
 
-def usage():
-    print "This is a very simple drop in python replacement for mail utilities commonly used with Nagios."
-    print "Usage: nagpymail.py -d sendto@foobar.com -f from@foobar.com -u mailuser -p mailpass -s serveraddress " \
-          "-t messagesubject -m messagebody"
-    print "Options:"
-    print "-d       Destination Address (Required)"
-    print "-f       From address (Required)"
-    print "-u       E-mail username (Required)"
-    print "-p       E-mail user password (Required)"
-    print "-s       Mail server address (Required)"
-    print "-t       Message subject (Required)"
-    print "-T       Enable TLS (Optional; default: disabled)"
-    print "-m       Message body (Required)"
-
-
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "d:f:m:p:s:t:Tu:")
-    except getopt.GetoptError as err:
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-T", help="Enable TLS", action="store_true", default=False)
+        required_args = parser.add_argument_group('required arguments')
+        required_args.add_argument("-d", help="Destination address", required=True, action="store", dest="to_address")
+        required_args.add_argument("-f", help="From address", required=True, action="store", dest="from_address")
+        required_args.add_argument("-u", help="Username to be used for mail server login", required=True,
+                                   action="store", dest="user_name")
+        required_args.add_argument("-p", help="User password to be used with mail server username", required=True,
+                                   action="store", dest="user_pass")
+        required_args.add_argument("-s", help="Mail server address", required=True, action="store",
+                                   dest="server_address")
+        required_args.add_argument("-t", help="E-mail message subject line", required=True, action="store",
+                                   dest="message_subject")
+        required_args.add_argument("-m", help="E-mail message body", required=True, action="store", dest="message_body")
+    except argparse.ArgumentError as err:
         print str(err)
-        usage()
         sys.exit(2)
 
-    enable_tls = False
-
-    for o, a in opts:
-        if o == "-d":
-            to_address = a
-        elif o == "-f":
-            from_address = a
-        elif o == "-u":
-            user_name = a
-        elif o == "-m":
-            message_body = a
-        elif o == "-p":
-            user_pass = a
-        elif o == "-s":
-            server_address = a
-        elif o == "-t":
-            message_subject = a
-        elif o == "-T":
-            enable_tls = True
-        else:
-            assert False, "Unhandled option " + o
+    args = parser.parse_args()
+    to_address = args.to_address
+    from_address = args.from_address
+    user_name = args.user_name
+    user_pass = args.user_pass
+    server_address = args.server_address
+    message_subject = args.message_subject
+    enable_tls = args.T
+    message_body = args.message_body
 
     # Message body uses HTML, will change options to allow for plaintext at later point.
-    try:
-        msg = MIMEText(message_body, 'html')
-        msg['Subject'] = message_subject
-        msg['From'] = from_address
-        msg['To'] = to_address
-        # We must set date timestamp, otherwise mail server/mail client may assume its own timestamp.
-        msg['Date'] = formatdate(localtime=True)
-        s = smtplib.SMTP(server_address, 587)
-        if enable_tls:
-            s.starttls()
-        s.login(user=user_name, password=user_pass)
-        s.sendmail(from_address, to_address, msg.as_string())
-    except UnboundLocalError as e:
-        print "Missing requirement: %s\n" % (e)
-        usage()
-        sys.exit(2)
+    msg = MIMEText(message_body, 'html')
+    msg['Subject'] = message_subject
+    msg['From'] = from_address
+    msg['To'] = to_address
+    # We must set date timestamp, otherwise mail server/mail client may assume its own timestamp.
+    msg['Date'] = formatdate(localtime=True)
+    s = smtplib.SMTP(server_address, 587)
+    if enable_tls:
+        s.starttls()
+    s.login(user=user_name, password=user_pass)
+    s.sendmail(from_address, to_address, msg.as_string())
 
 
 if __name__ == "__main__":
